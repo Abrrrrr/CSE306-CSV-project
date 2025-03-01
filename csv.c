4185a25e791c5af7fca9b99de39d9e9e7cddfa9f
Abrrr <nuruddinabrar123@gmail.com>
2025-02-28 23:29:22 -0500

    wave the white flag

--- csv.c
+++ csv.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <limits.h>
 
 bool h_is=false;
 char *field;
@@ -41,47 +42,106 @@ char *field;
 		int ret=0;
 		char *data =strtok(header,",");
 		while(data!=NULL){
-			if(strmp(data,field)==0){
+			if(strcmp(data,field)==0){
 				return ret;
 			}
 			ret++;
 			data=strtok(NULL,",");
-		return INT_MIN;
-	}
+	}		
+		return -1;
+	
+}
 
 
 
-	void min(char* filename, char* argv[]){
+	void min(char* filename, char *field){
 
 		FILE *file = fopen(filename,"r");
 		char header[4096];
 		int min=INT_MAX;
 
+		int fieldIndex=atoi(field);
+		if (fgets(header, sizeof(header), file)) {
 		if(h_is){
 			fgets(header,sizeof(header),file);
+			fieldIndex= fieldTranslate(header,field);
+			if(fieldIndex==-1){
+				exit(EXIT_FAILURE);
+			}else{
+				fieldIndex=atoi(field);
+			}
 		}
-		while(fget(header,sizeof(header),file)){
+		}
+		while(fgets(header,sizeof(header),file)){
 			char *data=strtok(header,",");
-			for(int i=0; i<)
+			for(int i=0; i<fieldIndex;i++){
+				data = strtok(NULL,",");
+			}
+		
+		if (data!= NULL){
+			if(atoi(data)<min){
+				min = atoi(data);
+			}
 		}
 
+		}
+		printf("%d\n",min);
 
+		fclose(file);
 	}
 
 
-	void max(char* filename, char* argv[]){
-		int max=INT_MIN;
-		return 0;
 
+
+
+	void max(char* filename, char *field){
+		int max=INT_MIN;
+		
+		FILE *file = fopen(filename,"r");
+		char header[4096];
+
+		int fieldIndex=atoi(field);
+		if (fgets(header, sizeof(header), file)) {
+		if(h_is){
+			fgets(header,sizeof(header),file);
+			fieldIndex= fieldTranslate(header,field);
+			if(fieldIndex==-1){
+				exit(EXIT_FAILURE);
+			}else{
+				fieldIndex=atoi(field);
+			}
+		}
+		}
+		while(fgets(header,sizeof(header),file)){
+			char *data=strtok(header,",");
+			for(int i=0; i<fieldIndex;i++){
+				data = strtok(NULL,",");
+			}
+		
+		if (data!= NULL){
+			if(atoi(data)>max){
+				max = atoi(data);
+			}
+		}
+
+		}
+		printf("%d\n",max);
+
+		fclose(file);
 	}
 
 	void mean(char* filename, char* argv[]){
 		int sum=0;
 		int count=0;
+			char*p="dog function, wave the white flag :(";
+	printf("%s",p);
 
-		return 0;
 	}
 
+	void records(char* filename, char* argv[]){
+	char*p="doodoo function, wave the white flag :(";
+	printf("%s",p);
+	}
 
 
 int main(int argc, char *argv[]){
@@ -109,20 +169,21 @@ int main(int argc, char *argv[]){
 		}else if(strcmp(argv[i],"-h")==0){
 			// run -h function
 			h_is= true;
-			fieldTranslate(header,)
 
-		}else if(strcmp(argv[i],"-min")==0){
+		}else if(strcmp(argv[i],"-min")==0 && i + 1 < argc - 1){
 			// run -min function
-
-		}else if(strcmp(argv[i],"-max")==0){
+			int j=i+1;
+			min(filename,argv[j]);
+		}else if(strcmp(argv[i],"-max" )==0&& i + 1 < argc - 1){
 			// run -max function
-
-		}else if(strcmp(argv[i],"-mean")==0){
+			int j=i+1;
+			max(filename,argv[j]);
+		}else if(strcmp(argv[i],"-mean" )==0){
 			// run -mean function
-
+			mean(filename,argv);
 		}else if(strcmp(argv[i],"-records")==0){
 			// run -records function
-
+			records(filename,argv);
 		}else{
 			return EXIT_FAILURE;
 		}
@@ -135,11 +196,5 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-
-
-	int records(char* filename, char* argv[]){
-
-		return 0;
-	}
 
 
